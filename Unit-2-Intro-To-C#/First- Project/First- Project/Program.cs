0140954a606227a164949dde41f51e09e3552e72
Missy <melissamichaux13@gmail.com>
2025-03-27 21:57:57 -0400

    First Commit

---
+++ Unit-2-Intro-To-C#/First- Project/First- Project/Program.cs
@@ -0,0 +1,50 @@
+﻿// This is a comment - everything after is ignored
+/* This is a block comment - everything between the slash-asterisk is ignored
+ This is useful if you have a lot to say or to deactivate code
+ */
+ //a namespace identifies a context in which a word is known
+//allows the same name to be used in different context
+//Everything in C# is defined in a Class.
+// A Class is a group of related things
+//A Class starts with the class keyword
+class Program
+//{ } surround blocks of code aka code-blocks
+ {
+  // Start of code for the class
+  // Every Application has a method called Main ()
+  // a method is a set of code that performs a function
+  // also known as a function or a program
+  // a method may return a value and accept data to process
+  // method signature identifies what the method returns it's name and what it accepts
+  //Main () is where every application starts
+  // Every application has one and only one Main() method
+  
+  // below Main()i a method that returns void and accepts string[] called args
+  //
+  // void means it returns nothing
+  // status means there can be one of these in the block or code (Program class)
+  
+  //   return
+  //   type name(data-it-accepts)
+  static void Main(string[] args) //method signature
+  {
+   // Start of the code for main ()
+   // This code will display whatever is between the ("") on the screen
+   
+   // Console is an object defined by C# to represent the screen
+   // An object has certain behaviors
+   // A behavior is something an object can do with data
+   // behaviors are implemented using methods
+   
+   //object.method(data) - object oriented programming
+   //
+   // when you see a . after a name, the name to left of the . is probably an object
+   //              name( , the name is a method
+   //              name after the ., the name is a method
+   // object.method(data-for-the-method-to-process)
+   
+   
+   Console.WriteLine("Hello, World!"); // EVERY C# STATEMENT ENDS WITH A ;
+   
+  } // End of the code for Main()
+ } // End of code for class
---
+++ Unit-2-Intro-To-C#/FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace FirstProgram;
+
+// The Program will ask for three number
+//     add them together and display the total
+
+// Programming Solution - Human Thinking
+// Identify the detailed steps
+//
+// Ask for the numbers one at a time
+// Write down each number as it's given
+// Add the numbers together to create a sum/total
+// Tell the total (display)
+//
+// Now that we have the steps in our solution 
+// Identify any data you need for the steps
+//
+// 1. A place to write down each number - a variable in a program
+// 2. A place to hold the sum/total - a variable in a program
+// 3. A way to ask for the numbers - a method in a program (object.method())
+// 4. A way to receive the numbers - a method in a program (object.method())
+// 5. A way to report the sum/total - a method in a program (object.method())
+//
+//Place the steps in the order and provide more detail if necessary 
+// 1. Ask for the numbers one at a time
+//   a. Tell whoever is giving us the number to give me the first number
+//   b. Receive the first number from the sender
+//   c. Write down each number 
+//   d. Tell whoever is giving us the number to give me the second number
+//   e. Receive the second number from the sender
+//   f. Write down second number 
+//   g. Tell whoever is giving us the number to give me the third number
+//   h. Receive the third number from the sender
+//   i. Write down third number 
+//
+// 2. Add the numbers together to create a sum/total
+//    a. Add the first number to a sum/total
+//    b. Add the second number to a sum/total
+//    c.Add the third number to a sum/total
+//
+// 3. Tell the requester the sum/total(display)
+//
+
+//Commonly used data types in C#
+// int - Numeric data that is a whole number between +/- 2 billion
+// long - A whole number greater than +/- 2 billion 
+//double - numeric value with decimal places
+// char - a single alpha-numeric character
+// string - a series o alpha-numeric characters (words or sentence)
+// bool - true or false for condition 
+
+// Variable names in C# are usually spelled in camelCase
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number1 = 0;
+        int number2 = 0;
+        int number3 = 0;
+        
+        int total = 0;
+        
+        Console.WriteLine("--- Starting Program ---");
+
+        Console.WriteLine("Please enter a number;"); //Asking for a number
+        string aLine; // String to hold the line of input from the keyboard
+        aLine = Console.ReadLine();
+        // we need an int to store our numbers
+        // Console.ReadLine() only returns a string
+        // So we need to convert the string from Console.ReadLine() to an int
+        // int.Parse(string) will convert a string to an int
+        
+        number1 = int.Parse(aLine);
+        
+        
+        
+        Console.WriteLine("--- Ending Program ---");
+    }
+}
