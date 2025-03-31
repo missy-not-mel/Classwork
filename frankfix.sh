7b6e1ea49f743f39c5c02ef09d06b9309ed6ca07
FFrank Fella <Frank@CyberAcademy.com>
2025-03-30 17:57:54 -0700

    Instructor Push 03/30/2025 17:57:53

---
+++ frankfix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if !(git rev-parse --is-inside-work-tree > /dev/null 2>&1;) then
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "* Not inside a Git working directory !"
+  echo "* Processing terminating             !"
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  exit
+fi
+  echo "*************************************"
+  echo "* Looks like you in a git directory *"
+  echo "* Processing continues              *"
+  echo "*************************************"
+  git config --global --unset pull.rebase
+  git remote remove frankscode
+  git remote add frankscode https://github.com/grandcircusco/CSharpAHBC-March-2025-Code.git
+  git config --global merge.tool "merge --no-edit"
+  git pull frankscode main --allow-unrelated-histories
+  git remote -v
+ 
