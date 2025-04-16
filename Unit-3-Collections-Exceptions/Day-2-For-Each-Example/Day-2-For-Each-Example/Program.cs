3c9e24a3d327296b935448d4fd1e6d9de23504d8
FFrank Fella <Frank@CyberAcademy.com>
2025-04-16 14:28:29 -0700

    Instructor Push 04/16/2025 14:28:28

---
+++ Unit-3-Collections-Exceptions/Day-2-For-Each-Example/Day-2-For-Each-Example/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day_2_For_Each_Example;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to an Example of a For-Each Loop");
+        
+        
+    }
+}
