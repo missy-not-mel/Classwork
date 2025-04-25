2ae559e5d2cbf4b955092e4af9898e817eb42660
FFrank Fella <Frank@CyberAcademy.com>
2025-04-25 04:58:33 -0700

    Instructor Push 04/25/2025 04:58:32

--- Unit-4-Intro-To-Object-Oriented-Programming/Day-3-Student-Class-Example-V3-Frank/Day-1-Student-Class-Example/Student.cs
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day-3-Student-Class-Example-V3-Frank/Day-1-Student-Class-Example/Student.cs
@@ -34,13 +34,13 @@ public class Student
     //       Class data should be initialized in constructors
     private string       studentName;
     private List<double> testScores;
-
+    
     // Define methods for the class
-
+    
     // One special methods for a class is called a constructor
     // A constructor is responsible for initializingthe data in a class
     // (data should never be uninitialized - the starting value needs to be known)
-
+    
     // a constructor method is special because:
     //
     //   1. it has no return type; not even void
@@ -48,10 +48,10 @@ public class Student
     //   3. it may or may not receive parameters (initializers)
     //      ( a constructor with no parameters is called a default constructor)
     //   4. Usually public 
-
+    
     // Define a constructor to initialize our data with values 
     //          specified by the user
-
+    
     // As the class Designer YOU decide what you need to properly initialize objects of the class
     // YOU decide how constructors you need or how users of the class can initialize your objects
     //
@@ -72,80 +72,28 @@ public class Student
 /********************************************************************************************
  * Constructors - Allow user to create object and initialize them
  *******************************************************************************************/
-
-    public Student(string theName) // 1-arg ctor to accept a name only
+    public Student(string theName)  // 1-arg ctor to accept a name only
     {
-        studentName = theName; // Assign the name passed to the ctor to our studentName
-        testScores = new List<double>(); // Define and assign an empty List to testscores
+        studentName = theName;            // Assign the name passed to the ctor to our studentName
+        testScores  = new List<double>(); // Define and assign an empty List to testscores
     }
-
-    public Student(string name, List<double> scores) // 2-arg constructor
-        // two parameters used to initialize an object
+    
+    public Student(string name, List<double> scores)  // 2-arg constructor
+                                                   // two parameters used to initialize an object
     {
-        studentName = name; // Set the class data to the data passed in from the user
-        testScores = scores; // Set the class data to the data passed in from the user
+        studentName = name;   // Set the class data to the data passed in from the user
+        testScores  = scores; // Set the class data to the data passed in from the user
     }
 
     /********************************************************************************************
-     * Getters and Setters to allow access to our private data
-     *
-     * Getters and Setters are special methods to allow a class controlled access to the data
-     *
-     * Getters - return the value in our data members
-     * Setters = allow the changing of data members
-     *
-     * By convention: Getters are named GetVariableName
-     *                Setters are named SetVariableName
-     *
-     * Most IDEs will generate standard Getters & Setters for any data already defined in the class
+     * Getters and Setters
      *******************************************************************************************/
 
-    public string GetStudentName()
-    {
-        return studentName;  // return the value in this private data member
-    }
-
-    public List<double> GetTestScores()
-    {
-        return testScores; // return the value in this private data member
-    }
-
-    public void SetStudentName(string newName)
-    {
-        studentName = newName;
-    }
-
-    public void SetTestScores(List<double> newScores)
-    {
-        testScores = newScores;
-    }
-
-
-/********************************************************************************************
- * Methods to manipulate the class
- *******************************************************************************************/
     
-    // We need to provide a ToString() method to return a string representation of the class data
-    // override - indicates you are a providing a method that overrides standard C# behavior
-    //            the word override is optional
-    // toString() should return a string representation of the data in the class
-    //            and accept no parameters
-    public override string ToString()
-    {
-        // Define a variable to hold the return value
-        string theData = "";
 
-        theData =  $"Name: {studentName}";   // Start with the student name in a string
-        theData += " Scores: ";
-        foreach (double aScore in testScores)
-        {
-            theData += aScore + " ";
-        }
-        
-        // return the variable with the result
-        return theData;
-    }
-
+    /********************************************************************************************
+     * Methods to manipulate the class
+     *******************************************************************************************/
 
     // We need a method to allow the user to add scores to our testScores List
     // Every method requires a method signature and a body
@@ -182,9 +130,12 @@ public class Student
     // Method compute average score for user
     public double AvgOfScores()
     {
-        // To round a double value to decimal places use Math.Round(value, 3-decimal-places)
-        return Math.Round(SumOfScores() / testScores.Count, 2); // Using a class method inside another class method
+        return SumOfScores() / testScores.Count; // Using a class method inside another class method
     }
+    
+    
+    
+    
     
     // Provide a method to display our data
     // (Console.WriteLine() doesn't know how to do it)
---
+++ Unit-4-Intro-To-Object-Oriented-Programming/PlayingCardProject-V1/PlayingCardProject/PlayingCard.cs
@@ -0,0 +1,31 @@
+﻿namespace PlayingCardProject;
+
+public class PlayingCard
+{
+    /******************************************************************************
+     * Data for the class - Instance Data
+     *
+     * Instance data means every object has it's own copy of the data
+     *******************************************************************************/
+
+    private string suit;
+    private string color;
+    //                      Ace                            Jack,Queen, King
+    private int    value; // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,  12,    13
+    
+    // Constructor for a PlayingCard - get all the values from the user
+    public PlayingCard(string suitName, string theColor, int theValue)
+    {
+        suit  = suitName;
+        value = theValue;
+        color = theColor;
+    }
+    
+    // ToString() so we can use a PlayingCard as string
+    public override string ToString()
+    {
+        return $"Value: {value}  Color: {color}  Suit: {suit}";
+    }
+        
+    
+}
