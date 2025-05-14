c2699cd7ddf14ad406e9abb00c667c56b3662322
FFrank Fella <Frank@CyberAcademy.com>
2025-05-14 15:12:51 -0700

    Instructor Push 05/14/2025 15:12:50

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day-7-Abstract-Classes-Frank/Day-7-Abstract-Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using GeneralPurposeFunctions;
+
+namespace Day_6_Abstract_Classes
+{
+class Program
+    {
+        private static CommonlyUsedFunctions commonCode = new CommonlyUsedFunctions();
+
+        static void Main(string[] args)
+        {
+            commonCode.WriteSeparatorLine("Welcome to the Abstract Class Example!");
+            
+            // If PlayingCard is abstract you cannot instantiate of the class
+            //PlayingCard aCard = new PlayingCard(1, "Hearts", "Red");
+            
+            AmericanPlayingCard usaCard       = new AmericanPlayingCard(1, "Hearts");
+            ItalianPlayingCard  italianCard   = new ItalianPlayingCard(10, "Cups");
+            SwissPlayingCard    swissCard     = new SwissPlayingCard(13, "Shields");
+            BlackJackCard       blackJackCard = new BlackJackCard(1, "Spades);");
+
+            usaCard.ShowCard();
+            blackJackCard.ShowCard();
+            swissCard.ShowCard();
+            italianCard.ShowCard();
+
+            commonCode.WriteSeparatorLine("Thanks for using the Abstract Class Example!");
+            commonCode.PauseProgram();
+
+        }
+    }
+}
---
+++ Unit-4-Intro-To-Object-Oriented-Programming/SampleOOPApplication-V1-Frank/SampleOOPApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SampleOOPApplication;
+
+class Program
+{
+    // Application program will define and mainpulate objects
+    static void Main(string[] args)
+    {
+        Console.WriteLine("------ Start of Sample Object Oriented Application Made From Scratch -------");
+        
+        Person aPerson = new Person("John", 78, 250.6, 69, "Male");
+        Console.WriteLine($"aPerson:  {aPerson}");  // .WriteLine() uses the ToString() of the class
+        
+        Person aPerson2 = new Person("John", 78, 250.6, 69, "Male");
+        Console.WriteLine($"aPerson2: {aPerson2}");  // .WriteLine() uses the ToString() of the class
+        
+        // Compare aPerson and aPerson2 to see if they are equal
+        // if (aPerson == aPerson2)   // == compares the reference value (locations in memory)
+        if (aPerson.Equals(aPerson2)) // .Equals() SHOULD compare the content of the object
+        {
+            Console.WriteLine("They are equal");
+        }
+        else
+        {
+            Console.WriteLine("They are NOT equal");
+        }
+        
+        Console.WriteLine("------ End of Sample Object Oriented Application Made From Scratch -------");
+    } // End of Main()
+}
