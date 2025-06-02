91143644a3fd65db6be44d80cc6f56fe9c313f4e
FFrank Fella <Frank@CyberAcademy.com>
2025-06-02 05:43:44 -0700

    Instructor Push 06/02/2025 05:43:43

---
+++ Unit-5-Java/00-Playing-Card-From-CSharp-V1-2/src/ApplicationProgram/TestPlayingCard.java
@@ -0,0 +1,9 @@
+package ApplicationProgram;
+
+public class TestPlayingCard {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    } // End of main() method
+} // End of TestPlayingCard CLass
